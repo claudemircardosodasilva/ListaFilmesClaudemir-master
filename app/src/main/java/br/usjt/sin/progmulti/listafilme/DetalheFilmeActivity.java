ea446e4f569e1ca590609ce24d7beab796508431
claudemircardosodasilva <claudemircardoso70@gmail.com>
2018-10-08 19:49:07 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/sin/progmulti/listafilme/DetalheFilmeActivity.java
@@ -0,0 +1,24 @@
+package br.usjt.sin.progmulti.listafilme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/*
+ *  Claudemir Cardoso da silva
+ *  RA: 816155452
+ */
+
+public class DetalheFilmeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_filme);
+        TextView nome = (TextView)findViewById(R.id.filme_selecionado);
+        Intent intent = getIntent();
+
+        nome.setText(intent.getStringExtra(ListarFilmesActivity.DESCRICAO));
+    }
+}
